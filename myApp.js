1190c579e837d3e0616d5edb1ba3ce350991b474
Daniyar Bashenov <bashenov98@gmail.com>
2023-02-15 23:15:56 +0600

    commit 11

--- myApp.js
+++ myApp.js
@@ -3,8 +3,6 @@ let app = express();
 
 console.log("Hello World");
 
-app.use(express.static(__dirname + "/public"));
-
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => {res.sendFile(__dirname + "/views/index.html");});
