6d5b806c73a62a08dd7c5b0d2bd581100310fcd2
Daniyar Bashenov <bashenov98@gmail.com>
2023-02-17 13:37:00 +0600

    commit 13

--- myApp.js
+++ myApp.js
@@ -1,13 +1,17 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 console.log("Hello World");
+
+var response = "Hello World";
 
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => {res.sendFile(__dirname + "/views/index.html");});
 
-app.get('/json', (req, res) => {res.json({"message": "Hello json"})});
+app.get('/json', (req, res) => 
+    {(process.env.VAR_NAME === "uppercase") ? res.json({"message": response.toUpperCase}) :res.json({"message": response})});
 
 
 
