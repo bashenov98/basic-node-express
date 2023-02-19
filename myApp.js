5c598df5212bb3b2dc879fcbcc24b68db8d99f1d
Daniyar Bashenov <bashenov98@gmail.com>
2023-02-19 23:05:34 +0600

    commit 15

--- myApp.js
+++ myApp.js
@@ -6,12 +6,17 @@ console.log("Hello World");
 
 var response = "Hello World";
 
+if (process.env.VAR_NAME === "uppercase") {
+    response = "Hello World".toUpperCase();
+  } else {
+    response = "Hello World";
+  }
+
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => {res.sendFile(__dirname + "/views/index.html");});
 
-app.get('/json', (req, res) => 
-    {(process.env.MESSAGE_STYLE === "uppercase") ? res.json({"message": response.toUpperCase}) :res.json({"message": response})});
+app.get('/json', (req, res) =>  {res.json({"message": response})});
 
 
 
